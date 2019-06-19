de2a291c2c2a3c28de1be4da435e852b6dddb5cd
sanw <33028688+wusanwan@users.noreply.github.com>
2019-06-19 23:21:39 +0800

    Add files via upload

--- Advanced Level/1121 Damn Single.cpp
+++ Advanced Level/1121 Damn Single.cpp
@@ -1,37 +1,49 @@
 //1121 Damn Single
 #include<cstdio>
 #include<vector>
+#include<iostream>
 #include<cstdlib>
 #include<set>
 #include<map>
+#include<string>
 #include<algorithm>
 #pragma warning(disable:4996)
 using namespace std;
+//如果用int存，map默认为1，要避免0也来了？
 int main(){
-	map<int, int>mp;
-	int couples, guest, temp,x,y;
-	scanf("%d", &couples);
+	map<string, string>mp;
+	int couples, guest;
+	string x, y;
+	cin>>couples;
 	for (int i = 0; i < couples; i++){//存储情侣
-		scanf("%d %d", &x, &y);
+		cin >> x >> y;
 		mp[x] = y;
 		mp[y] = x;
 	}
-	scanf("%d", &guest);
-	vector<int>judge;
-	set<int>dog;
-	for (int i = 0; i < guest; i++){
-		scanf("%d", &temp);
-		judge.push_back(temp);
+	cin>>guest;
+	vector<string>judge;
+	int arrive[100000] = { 0 };
+	set<string>dog;
+	string temp;
+	int tempint;
+	for (int i = 0; i < guest; i++){//读入客人
+		cin >> temp;
+		judge.push_back(temp);//加入客人清单
+		tempint=stoi(temp);
+		arrive[tempint] = 1;//加入签到表
 	}
-	for (auto it = judge.begin(); it != judge.end(); it++){
-		if (mp[*it] != 0 && find(judge.begin(), judge.end(), mp[*it]) == judge.end())//有伴侣但是伴侣没来
-			dog.insert(*it);
-		if (mp[*it] == 0)dog.insert(*it);//无伴侣
+	for (auto it = judge.begin(); it != judge.end(); it++){//判断客人有无伴侣
+		if (mp[*it] == "")dog.insert(*it);//单身
+		else{//有伴侣但没来
+			int spouse = stoi(mp[*it]);
+			if (!arrive[spouse])//有伴侣但是伴侣没来或无伴侣
+				dog.insert(*it);
+		}
 	}
-	printf("%d\n", dog.size());
+	cout<<dog.size()<<endl;
 	for (auto it = dog.begin(); it != dog.end(); it++){
-		if (it != dog.begin())printf(" ");
-		printf("%d", *it);
+		if (it != dog.begin())cout<<" ";
+		cout<<*it;
 	}
 	system("pause");
 	return 0;
